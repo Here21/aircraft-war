d4beaafe3faba86c9f71706b1e03318fe0968d29
Here21 <deletetmr@gmail.com>
2017-11-08 22:17:03 +0800

    🎁 掉落道具

---
+++ assets/Script/Game/ufo.js
@@ -0,0 +1,29 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        speedMax: 0,
+        speedMin: 0,
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        // 速度随机[speedMax, speedMin]
+        this.speed = Math.random() * (this.speedMax - this.speedMin + 1) + this.speedMin;
+
+        let manager = cc.director.getCollisionManager();
+        manager.enabled = true;
+    },
+    //碰撞检测
+    onCollisionEnter: function(other, self){
+        this.ufoGroup.destroyUfo(this.node);
+    },
+    // called every frame, uncomment this function to activate update callback
+    update: function (dt) {
+        this.node.y -= dt * this.speed;
+        //出屏幕后
+        if (this.node.y < -this.node.parent.height / 2) {
+            this.ufoGroup.destroyUfo(this.node);
+        }
+    },
+});
---
+++ assets/Script/Game/ufoGroup.js
@@ -0,0 +1,74 @@
+const ufoG = cc.Class({
+   name: 'ufoG',
+   properties: {
+       name: '',
+       prefab: cc.Prefab,
+       freq: 0,
+       poolAmount: 0,
+       delayMax: {
+           default: 0,
+           tooltip: '最大延时'
+       },
+       delayMin: {
+           default: 0,
+           tooltip: '最小延时'
+       },
+   }
+});
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        ufoG: {
+            default: [],
+            type: ufoG
+        }
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        D.common.batchInitNodePool(this, this.ufoG);
+        this.startAction();
+    },
+    // 填充弹药
+    startAction: function () {
+        for(let i = 0; i < this.ufoG.length; i++) {
+            let ufoName = this.ufoG[i].name;
+            let freq = this.ufoG[i].freq;
+            this[ufoName] = function (ii) {
+                let delay = Math.random() * (this.ufoG[ii].delayMax - this.ufoG[ii].delayMin) + this.ufoG[ii].delayMin;
+                // 内存定时器，随机掉落时间
+                this.scheduleOnce(function() {
+                    this.genNewUfo(this.ufoG[ii]);
+                }.bind(this), delay);
+            }.bind(this, i);
+            // 外层定时器，循环掉落
+            this.schedule(this[ufoName], freq);
+        }
+    },
+    // 生成ufo
+    genNewUfo: function (ufoInfo) {
+        let poolName = ufoInfo.name + 'Pool';
+        let newNode = D.common.genNewNode(this[poolName], ufoInfo.prefab, this.node);
+        let pos = this.getNewEnemyPosition(newNode);
+        newNode.setPosition(pos);
+        newNode.getComponent('ufo').ufoGroup = this;
+    },
+    //随机生成的位置
+    getNewEnemyPosition: function(newEnemy) {
+        //位于上方，先不可见
+        var randx = cc.randomMinus1To1() * (this.node.parent.width / 2 - newEnemy.width / 2);
+        var randy = this.node.parent.height / 2 + newEnemy.height / 2;
+        return cc.v2(randx,randy);
+    },
+    // 销毁
+    destroyUfo: function (node) {
+        D.common.putBackPool(this, node);
+    }
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
